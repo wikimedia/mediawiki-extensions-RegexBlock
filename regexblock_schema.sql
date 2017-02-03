f77955f36f97364e9c57e15ef4be04a3d4dafe2a
Jack Phoenix <jack@countervandalism.net>
2017-02-03 03:43:56 +0200

    Quick SQLite port of the DB schema
    Change-Id: I8f3b71218b4774b0b3729c5acbab36970d80ba8d

--- regexblock_schema.sql
+++ regexblock_schema.sql
@@ -1,26 +1,28 @@
 CREATE TABLE /*_*/blockedby (
-	blckby_id int(5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	blckby_id int(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	blckby_name varchar(255) NOT NULL,
 	blckby_blocker varchar(255) NOT NULL,
 	blckby_timestamp char(14) NOT NULL,
 	blckby_expire char(14) NOT NULL,
 	blckby_create tinyint(1) NOT NULL default '1',
 	blckby_exact tinyint(1) NOT NULL default 0,
-	blckby_reason tinyblob NOT NULL,
-	UNIQUE KEY blckby_name (blckby_name),
-	KEY blckby_timestamp (blckby_timestamp),
-	KEY blckby_expire (blckby_expire)
+	blckby_reason tinyblob NOT NULL
 )/*$wgDBTableOptions*/;
 
+CREATE UNIQUE INDEX /*i*/blckby_name ON /*_*/blockedby (blckby_name);
+CREATE INDEX /*i*/blckby_timestamp ON /*_*/blockedby (blckby_timestamp);
+CREATE INDEX /*i*/blckby_expire ON /*_*/blockedby (blckby_expire);
+
 CREATE TABLE /*_*/stats_blockedby (
-	stats_id int(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	stats_id int(8) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	stats_blckby_id int(8) NOT NULL,
 	stats_user varchar(255) NOT NULL,
 	stats_blocker varchar(255) NOT NULL,
 	stats_timestamp char(14) NOT NULL,
 	stats_ip char(15) NOT NULL,
 	stats_match varchar(255) NOT NULL default '',
-	stats_dbname varchar(255) NOT NULL default '',
-	KEY stats_blckby_id_key (stats_blckby_id),
-	KEY stats_timestamp (stats_timestamp)
+	stats_dbname varchar(255) NOT NULL default ''
 )/*$wgDBTableOptions*/;
+
+CREATE INDEX /*i*/stats_blckby_id_key ON /*_*/stats_blockedby (stats_blckby_id);
+CREATE INDEX /*i*/stats_timestamp ON /*_*/stats_blockedby (stats_timestamp);
